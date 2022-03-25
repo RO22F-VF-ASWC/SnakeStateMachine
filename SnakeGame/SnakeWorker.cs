868e54142db1f4125c751e54842857b89e7690d0
Peter Levinsky <pele@easj.dk>
2022-03-25 12:59:41 +0100

    Add project files.

---
+++ SnakeGame/SnakeWorker.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Security.Cryptography;
+using SnakeLib.snake;
+using SnakeLib.state;
+
+namespace SnakeGame
+{
+    internal class SnakeWorker
+    {
+        public void Start()
+        {
+            SnakePlayground pg = new SnakePlayground(20,20);
+            
+            // using table state machine
+            IState stateMachine = new SnakeTableStateMachine();
+
+            // using state machine pattern
+            //IState stateMachine = new SnakeStateMachinePattern();
+
+            bool gameContinue = true;
+            pg.PrintPlayground();
+
+            while (gameContinue)
+            {
+                Console.WriteLine("Point = " + pg.Point);
+
+                InputType nextInput = ReadNextEvent();
+                Move nextMove = stateMachine.NextMove(nextInput);
+
+                gameContinue = pg.DoNextMove(nextMove);
+                if (gameContinue)
+                {
+                    pg.PrintPlayground();
+                }
+                
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("You loose :-( ");
+
+        }
+
+
+        private InputType ReadNextEvent()
+        {
+            InputType ev = InputType.FORWARD;
+
+            Console.Write("Type next movement 'a,w,d' : ");
+            bool ok = false;
+            while (!ok)
+            {
+                ConsoleKeyInfo info = Console.ReadKey();
+                char c = info.KeyChar;
+                switch (c)
+                {
+                    case 'a':
+                        ev = InputType.LEFT;
+                        ok = true;
+                        break;
+                    case 'd':
+                        ev = InputType.RIGHT;
+                        ok = true;
+                        break;
+                    case 'w':
+                        ev = InputType.FORWARD;
+                        ok = true;
+                        break;
+                }
+            }
+
+            return ev;
+        }
+    }
+}
